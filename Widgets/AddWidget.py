9c8785a1ea55081ca5af8c6dd8354739d5a3bd86
vorop <2vvoropaevv2@gmail.com>
2023-11-19 13:53:07 +0700

    Add app header

---
+++ Widgets/AddWidget.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import QRegularExpression
+from PyQt5.QtGui import QIntValidator, QRegularExpressionValidator
+from PyQt5.QtWidgets import QLineEdit, QSizePolicy
+
+
+def create_line(lay_for_adding, line: QLineEdit, obj_name: str = None,
+                label: str = "i'm label", reg_text: str = None, is_set_policy=True):
+    if reg_text == "IntV":
+        validator = QIntValidator(0, 100)
+        line.setValidator(validator)
+    elif not (reg_text is None):
+        reg_ex = QRegularExpression(reg_text)
+        validator = QRegularExpressionValidator(reg_ex, line)
+        line.setValidator(validator)
+    if is_set_policy:
+        line.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed))
+    line.setObjectName(obj_name)
+    lay_for_adding.addRow(label, line)
---
+++ Widgets/InputView.py
@@ -0,0 +1,71 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtWidgets import QWidget, QLineEdit, QFormLayout, QVBoxLayout, QLabel, QPushButton
+
+from AddWidget import create_line
+
+
+class InputView(QWidget):
+    class EditLine(QLineEdit):
+        def __init__(self):
+            super().__init__()
+
+        def keyPressEvent(self, e: QKeyEvent):
+            if not e.text() in self.text():
+                super().keyPressEvent(e)
+
+    class NecessaryChain(QLineEdit):
+
+        def __init__(self, parent=None):
+            super().__init__(parent)
+
+        def keyPressEvent(self, a0):
+            text: QLineEdit = self.parent().findChild(QLineEdit, "symb")
+            if a0.text() in text.text() or a0.key() == QtCore.Qt.Key.Key_Backspace:
+                super().keyPressEvent(a0)
+
+    class Multiplicity(QLineEdit):
+
+        def __init__(self, parent=None):
+            super().__init__(parent)
+
+        def keyPressEvent(self, a0):
+            if not (a0.text() == "0" and self.text() == ""):
+                super().keyPressEvent(a0)
+
+    def __init__(self, parent=...):
+        super().__init__(parent)
+
+        main_lay = QVBoxLayout()
+        main_lay.addWidget(QLabel("Alphabet"))
+        lay: QFormLayout = QFormLayout()
+        create_line(lay, self.EditLine(), "symb", "available symbols:", r"\w+")
+        create_line(lay, self.NecessaryChain(), "subchain", "Necessary subchain:", r"\w+")
+        create_line(lay, self.Multiplicity(), "mult", "Multiplicity:", "IntV")
+        main_lay.addLayout(lay)
+        self.button = QPushButton("Setup")
+        self.button.setObjectName("setup")
+        main_lay.addWidget(self.button)
+        self.input_info = QLabel()
+        self.input_info.setObjectName("log_dka")
+
+        self.input_info.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        main_lay.addWidget(self.input_info)
+        main_lay.addStretch()
+        self.setLayout(main_lay)
+
+    @QtCore.pyqtSlot()
+    def check_input(self, sym: str, mult: str) -> bool:
+        if sym != "" and mult != "":
+            return False
+
+        if sym == "":
+            info = "available characters must not be empty"
+            self.input_info.setText(info)
+        elif mult == "":
+            info = "multiplicity must not be empty"
+            self.input_info.setText(info)
+        return True
+
+
